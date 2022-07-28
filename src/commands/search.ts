99d50e0f1335b0166ebe4f90629ddd2583d629b4
Mike Donnalley <mdonnalley@salesforce.com>
2022-07-28 10:35:33 -0600

    feat: add search command

---
+++ src/commands/search.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import {prompt, registerPrompt} from 'inquirer'
+import {Command, Interfaces, toConfiguredId, toStandardizedId} from '@oclif/core'
+import {AutocompleteSearch, Choices} from '../util/autocomplete'
+
+export default class Search extends Command {
+  public async run(): Promise<unknown> {
+    const commands = this.config.commands as Array<Interfaces.Command.Loadable & { readableId: string }>
+    for (const command of commands) {
+      command.readableId = toConfiguredId(command.id, this.config)
+    }
+
+    registerPrompt('autocomplete-search', AutocompleteSearch)
+
+    const {command} = await prompt<{ command: string }>([
+      {
+        type: 'autocomplete-search',
+        name: 'command',
+        suggestOnly: true,
+        message: 'Search for a command',
+        emptyText: 'Nothing found!',
+        pageSize: 10,
+        validate: (val: string): string | boolean => {
+          return val ? true : 'Type something!'
+        },
+        searchOpts: {
+          keys: ['readableId'],
+          shouldSort: true,
+          threshold: 0.25,
+          ignoreLocation: true,
+        },
+        displayKey: 'readableId',
+        options: commands,
+        footer: (currentChoices: Choices, index: number): string | undefined => {
+          const cmd = commands.find(c => c.readableId === currentChoices.getChoice(index).value)
+          if (cmd && cmd.summary) return cmd.summary
+        },
+      },
+    ])
+
+    if (command === 'help') {
+      return this.config.runCommand(toStandardizedId(command, this.config), [])
+    }
+
+    return this.config.runCommand('help', [toStandardizedId(command, this.config)])
+  }
+}
